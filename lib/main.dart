21f008ac6ef408c8ac3750a0da2ad87a159211ca
ukkari <whoislanz@gmail.com>
2026-02-04 17:26:08 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart'; 
 
 void main() => runApp(MaterialApp(
+  title: 'Enzeiverse Engine', // Nama aplikasi di sistem
   debugShowCheckedModeBanner: false, 
   home: WebViewApp()
 ));
